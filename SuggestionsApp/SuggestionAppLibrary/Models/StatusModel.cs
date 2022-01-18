2ef98543261ae726ffb766e87f20c09b04d86a08
Ana Helena <ahelena-magit@vortx.com.br>
2022-01-18 10:04:14 -0300

    chore: data model creation

---
+++ SuggestionsApp/SuggestionAppLibrary/Models/StatusModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class StatusModel
+{
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string StautsName { get; set; }
+   public string StautsDescription { get; set; }
+}
--- SuggestionsApp/SuggestionAppLibrary/Models/SuggestionModel.cs
+++ SuggestionsApp/SuggestionAppLibrary/Models/SuggestionModel.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SuggestionAppLibrary.Models;
+﻿namespace SuggestionAppLibrary.Models;
 
 public class SuggestionModel
 {
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string Suggestion { get; set; }
+   public string Description { get; set; }
+   public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+   public CategoryModel Category { get; set; }
+   public BasicUserModel Author { get; set; }
+   public HashSet<string> UserVotes { get; set; } = new(); //List with unique values
+   public StatusModel SuggestionStatus { get; set; }
+   public string OwnerNotes { get; set; }
+   public bool ApprovedForReleased { get; set; } = false;
+   public bool Archived { get; set; } = false;
+   public bool Rejected { get; set; } = false;
 }
