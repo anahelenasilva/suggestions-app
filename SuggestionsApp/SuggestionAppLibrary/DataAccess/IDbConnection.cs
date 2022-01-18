2c94495fcb99fb1a1d6e33c1c40d1b00b784ac68
Ana Helena <ahelena-magit@vortx.com.br>
2022-01-18 12:52:07 -0300

    feat: add user data access

---
+++ SuggestionsApp/SuggestionAppLibrary/DataAccess/IDbConnection.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public interface IDbConnection
+{
+   IMongoCollection<CategoryModel> CategoryCollection { get; }
+   string CategoryColletionName { get; }
+   MongoClient Client { get; }
+   string DbName { get; }
+   IMongoCollection<StatusModel> StatusCollection { get; }
+   string StatusColletionName { get; }
+   IMongoCollection<SuggestionModel> SuggestionCollection { get; }
+   string SuggestionColletionName { get; }
+   IMongoCollection<UserModel> UserCollection { get; }
+   string UserColletionName { get; }
+}
