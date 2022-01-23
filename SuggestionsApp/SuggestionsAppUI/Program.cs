abb82e06e4113a1d22273ab292b7b6579c6267d7
Ana Helena <anahelenarp@hotmail.com>
2022-01-23 16:33:38 -0300

    feat: add logout redirect

--- SuggestionsApp/SuggestionsAppUI/Program.cs
+++ SuggestionsApp/SuggestionsAppUI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Rewrite;
 using SuggestionsAppUI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +10,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+   app.UseExceptionHandler("/Error");
+   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -22,6 +23,16 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseRewriter(
+   new RewriteOptions().Add(context =>
+   {
+      if (context.HttpContext.Request.Path == "/MicrosoftIdentity/Account/SignedOut")
+      {
+         context.HttpContext.Response.Redirect("/");
+      }
+   })
+);
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
